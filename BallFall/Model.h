f3cba6e79a5177beaa30dc3f2930c35b0b454917
Wiktor Biruk <wbiruk@wp.pl>
2015-07-28 16:54:17 +0200

    chyba działa dobrze

---
+++ BallFall/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  BallFall
+//
+//  Created by Wiktor Biruk on 28/07/15.
+//  Copyright (c) 2015 Wiktor Biruk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ BallFall/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  BallFall
+//
+//  Created by Wiktor Biruk on 28/07/15.
+//  Copyright (c) 2015 Wiktor Biruk. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
