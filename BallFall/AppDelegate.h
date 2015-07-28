e063d8d22f1051fb22d553c1e36b3280dfee315f
Wiktor Biruk <wbiruk@wp.pl>
2015-07-28 11:51:39 +0200

    Initial Commit

---
+++ BallFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BallFall
+//
+//  Created by Wiktor Biruk on 28/07/15.
+//  Copyright (c) 2015 Wiktor Biruk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
